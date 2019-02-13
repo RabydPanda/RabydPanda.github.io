5421a47a96e4008624271ce020472bd28cce4796
RabydPanda <35562651+RabydPanda@users.noreply.github.com>
2019-02-13 21:50:20 +1300

    Update teammembers.js

--- js/teammembers.js
+++ js/teammembers.js
@@ -22,14 +22,14 @@ var livecheckoptions = { method: 'GET',
 teamcallback = function (error, response, body) {
     if (error) throw new Error(error);
 
-	var $team = $('#team-members');
-	$team.children().remove();
+//	var $team = $('#team-members');
+//	$team.children().remove();
 
     var parsedTeamData = JSON.parse(body);
 
 	parsedTeamData.users.forEach(function (displayname){
-		var $div = $('<div>' + displayname);
-		$team.append($div);
+//		var $div = $('<div>' + displayname);
+//		$team.append($div);
 		};
 };
 
