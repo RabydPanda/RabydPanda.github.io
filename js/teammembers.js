9b77bc014b75493bff09ac0fc50de7da4a99efed
RabydPanda <35562651+RabydPanda@users.noreply.github.com>
2019-02-14 03:18:02 +1300

    Update teammembers.js

--- js/teammembers.js
+++ js/teammembers.js
@@ -17,8 +17,9 @@ var livecheckoptions = { method: 'GET',
         	'Accept': 'application/vnd.twitchtv.v5+json',
             'Client-ID': 'uflpzu6capd5fy7q1eliihjz8dd1yd' } };
 
-$(document).ready(function() {
     $('#team-name').append('WHOOPSY');
+
+$(document).ready(function() {
 fs.readFile('../JSON/config.json', 'utf-8', function (err, data) {
     json.parse(data);
     $('#team-name').append(data.teamname);
